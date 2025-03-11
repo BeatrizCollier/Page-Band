30301505a4381110c0fc880e40f5f1b038da024c
BeatrizCollier <beatrizmcollier@hotmail.com>
2025-03-11 17:32:45 -0300

    add:Adicionei o Hedear, Footer e o Aside.

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header/Header"; 
+import Aside from "../../components/Aside/Aside";
+import Footer from "../../components/Footer/Footer";
+
+function Home(){
+    return(
+        <>
+            <Header />
+            <Aside />
+            <Footer />
+        </>
+    )
+}
+
+export default Home;

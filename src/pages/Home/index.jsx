a15f609687576f40cc1973ca826cc68026d08e75
BeatrizCollier <beatrizmcollier@hotmail.com>
2025-03-12 17:32:00 -0300

    add:Adicionei Main e o index.css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "../../components/Header/Header"; 
 import Aside from "../../components/Aside/Aside";
+import Main from "../../components/Main/Main";
 import Footer from "../../components/Footer/Footer";
+
+
 
 function Home(){
     return(
         <>
             <Header />
+            <Main />
             <Aside />
             <Footer />
+
         </>
     )
 }
